4eed88e546e9dc5095176e3f638cac640a33e63d
mourisnicolas <125296096+mourisnicolas@users.noreply.github.com>
2023-02-25 00:19:54 +0100

    Add files via upload

---
+++ pages/api/idid/index.js
@@ -0,0 +1,25 @@
+import dbConnect from "../../../lib/dbConnect";
+import Product from "../../../models/product";
+
+async function handler(req, res) {
+  const { method, body } = req;
+  await dbConnect();
+
+  switch (method) {
+    case "POST":
+      try {
+        await Product.findOneAndUpdate({_id: body.data}, {$set: {id: body.data}});
+
+        return res.status(200).send("niceee");
+      } catch (error) {
+        console.log(error)
+        return res.status(400).send("Produsul nu a putut sa fie gasit");
+      }
+    default:
+      break;
+  }
+
+}
+
+
+export default handler;
